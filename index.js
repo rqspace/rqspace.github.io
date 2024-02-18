e52c7b0554d237808e2809f1cbfb36faef288cb9
Qiang Ren <67825907+rqspace@users.noreply.github.com>
2024-02-18 13:26:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var Parser = require('rss-parser');
 const rssParser = new Parser();
 async function start() {
     const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
-    if (Date.now() - new Date(blogResp.lastBuildDate).getTime() > 48 * 60 * 60 * 1000) {
-        console.log('最近48小时内没有博客更新，跳过本次更新');
-        return;
-    }
+    //if (Date.now() - new Date(blogResp.lastBuildDate).getTime() > 48 * 60 * 60 * 1000) {
+    //    console.log('最近48小时内没有博客更新，跳过本次更新');
+    //    return;
+    //}
     const items = [...blogResp.items.slice(0, 5)];
     const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
     const readme = await fs.readFile('README.md', 'utf-8');
