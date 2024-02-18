0081d7697e9ceb89b7f6f71ec328b39922ceb51a
Qiang Ren <67825907+rqspace@users.noreply.github.com>
2024-02-18 17:01:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ async function start() {
     //    console.log('最近48小时内没有博客更新，跳过本次更新');
     //    return;
     //}
+    
+    /*
     const items = [...blogResp.items.slice(0, 5)];
     const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
     const readme = await fs.readFile('README.md', 'utf-8');
     const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
     await fs.writeFile('README.md', newReadme);
     console.log('更新博客链接成功');
+    */
+    const hotboard = await fetch('https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc').then(res=> res.text());
+    const readme = await fs.writeFile('./json/tt-hotboard.json', hotboard);
 }
 start();
