3c24cdb52d9834d401d7e7f5d3e0350020070758
RQ <67825907+rqspace@users.noreply.github.com>
2026-02-13 03:59:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ async function start() {
     //    return;
     //}  
 
-    const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
-    const items = [...blogResp.items.slice(0, 5)];
-    const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
-    const readme = await fs.readFile('README.md', 'utf-8');
-    const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
-    await fs.writeFile('README.md', newReadme);
-    console.log('更新博客链接成功');
+    // const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
+    // const items = [...blogResp.items.slice(0, 5)];
+    // const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
+    // const readme = await fs.readFile('README.md', 'utf-8');
+    // const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
+    // await fs.writeFile('README.md', newReadme);
+    // console.log('更新博客链接成功');
     
-    const hotboard = await fetch('https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc').then(res=> res.text());
-    await fs.writeFile('./json/tt-hotboard.json', hotboard);
+    // const hotboard = await fetch('https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc').then(res=> res.text());
+    // await fs.writeFile('./json/tt-hotboard.json', hotboard);
 }
 start();
+
